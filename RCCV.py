7ed235ebbca1016668c418fdff4869ee8021ed30
Mrf <me+github@folmer.info>
2024-01-27 14:17:57 +1100

    first release

--- RCCV.py
+++ RCCV.py
@@ -19,20 +19,84 @@ import matplotlib.pyplot as plt
 import sys
 import json
 import crossref_commons.retrieval
-
+import argparse
+import os.path
+import logging
+import itertools 
 
 from pybtex.database import parse_file
 from contextlib import suppress
 
+
+logger = logging.getLogger("RCCV_logger")
 API_URL_BASE_SS = "https://api.semanticscholar.org/graph/v1/"
 
+'''
+The main function handles the user inputs.
+Format RCCV.py [-h] [-s src] filename
 
+'''
 def main():
+	# initialize handler
+	logger.addHandler(logging.StreamHandler())
+	# CLI
+	parser = argparse.ArgumentParser(description="models references")
+	parser.add_argument("filename", help="BibTex file to be used")
+	parser.add_argument("-s", "--source", type = str, metavar = "src", default = "ss", help = "use \"ss\" for SemanticScholar or \"cr\" for CrossRef")
+	parser.add_argument("-v", "--verbose", action="store_true", help = "show INFO logging")
+	parser.add_argument("-f", "--force", action="store_true", help = "don't get results from cache")
+	parser.add_argument("-a", "--all", action="store_true", help = "also shows referenced works not in the bibtex file")
+	parser.add_argument("-m", "--max", type = int, metavar = "max_nr", default=10, help = "maximum nodes not in bibtex file(default 10)")
+	parser.add_argument("-t", "--threshold", type = int, metavar = "thr_nr", default=3, help = "only include node not in bibtex file if at least thr_nr references")
+	args = parser.parse_args()
+
+	# check if file exists
+	if not os.path.exists(args.filename):
+		logger.error("please specify an existing file!")
+		raise SystemExit(1)
+
+	# check if SemanticScholar or CrossRef is specified
+	if args.source != "ss" and args.source != "cr":
+		logger.error("please specify a valid source: ss(SemanticScholar) or cr(CrossRef)")
+		raise SystemExit(1)
+
+	# check if numbers are within the ranges
+	if args.max < 0 or args.max > 64 or args.threshold < 1 or args.threshold > 256:
+		logger.error("please specify a max and threshold within the thresholds 0-64 and 1-256")
+		raise SystemExit(1)
+
+	# set verbose logging
+	if args.verbose:
+		logger.setLevel(logging.INFO)
+		logger.info("setting logging level to INFO")
+	else:
+		logger.setLevel(logging.WARNING)
+
+	# set cache if force is not active
+	if not args.force:
+		# cache expires after a week
+		requests_cache.install_cache(cache_name='RCCV_cache', backend='sqlite', expire_after=604800)
+		logger.info("enabling cache")
+
+	# log settings to info
+	logger.info("""\nusing the following settings:
+	filename: %s
+	source: %s
+	verbose: %s 
+	no cache: %s
+	show all results: %s
+	maximum non-list nodes: %d
+	threshold non-list nodes: %d""" % (args.filename, "SemanticScholar" if args.source == "ss" else "CrossRef", args.verbose, args.force, args.all, args.max, args.threshold))
+
+	papers = parse_papers(args.filename, source=args.source)
+	visualize(papers, plot_everything=args.all, threshold=args.threshold, maximum=args.max)
+
+	'''
 	if len(sys.argv) >= 2 and len(sys.argv) <= 4:
 		crossref = "semanticscholar"
 		using_all = False
 		# cache expires after a week
-		requests_cache.install_cache(cache_name='semanticscholar_cache', backend='sqlite', expire_after=604800)
+		requests_cache.install_cache(cache_name='RCCV_cache', backend='sqlite', expire_after=604800)
 		print("You specified file %s" % sys.argv[1])
 		if len(sys.argv) >= 3 and sys.argv[2] == "CrossRef":
 			print("Using CrossRef")
@@ -48,6 +112,7 @@ def main():
 		visualize(papers, plot_everything=using_all)
 	else:
 		print("Usage: python3 RCCV.py [filename] [CrossRef/SemanticScholar] [ALL]")
+	'''
 
 
 '''
@@ -156,25 +221,27 @@ returns a set of papers with the fields:
 * year
 * ...
 '''
-def parse_papers(file, source="semanticscholar"):
+def parse_papers(file, source="ss"):
 	papers = []
 	try:
 		bib_data = parse_file(file)
 		for entry_key in bib_data.entries:
 			entry = bib_data.entries[entry_key]
 			try:
-				if source == "crossref":
+				if source == "cr":
+					logger.info("Using CrossRef")
 					papers.append(get_metadata_crossref(entry.fields["doi"]))
 				else:
+					logger.info("Using SemanticScholar")
 					papers.append(get_metadata_semanticscholar(entry.fields["doi"]))
 			except KeyError:
 				try:
-					print("Could not find DOI for %s. Going to try to get DOI from title" % entry.fields["title"])
+					logger.error("Could not find DOI for %s. Going to try to get DOI from title" % entry.fields["title"])
 					# TODO
 				except KeyError:
-					print("Ignored %s because we could not find a title" % entry)
+					logger.error("Ignored %s because we could not find a title" % entry)
 	except Exception as e: 
-		print("Make sure to specify a correct bibtex file!\nException %s" % e)
+		logger.error("Make sure to specify a correct bibtex file!\nException %s" % e)
 	return papers
 
 
@@ -182,45 +249,70 @@ def parse_papers(file, source="semanticscholar"):
 '''
 if plot_everything is enabled we also plot the papers referenced but not in our list that are at least cited threshold times
 '''
-def visualize(papers, plot_everything=False, threshold=3):
+def visualize(papers, plot_everything=False, threshold=3, maximum=10):
 	G = nx.DiGraph()
 	# all papers in our list
 	papers_ind = [paper['id'] for paper in papers]
 	# all papers referenced but not in our list
 	nonlist_papers_ind = []
 
-	# add edges
+	# get all unique papers cited by our list and store the counts as well (only when plot_everything=True)
+	nonlist_papers = {}
+	if plot_everything:
+		for paper in papers:
+			for reference in paper["references"]:
+				# should not be in our list of papers
+				if reference["id"] not in papers_ind:
+					# does not yet exist
+					if reference["id"] not in nonlist_papers:
+						nonlist_papers[reference["id"]] = reference
+						# make sure this field does not already exist!
+						nonlist_papers[reference["id"]]["mycount"] = 1
+					else:
+						nonlist_papers[reference["id"]]["mycount"] += 1
+		# we need at most maximum nr entries and all should have >= threshold
+		try:
+			nonlist_papers = {x: nonlist_papers[x] for x in nonlist_papers if nonlist_papers[x]['mycount'] >= threshold}
+			nonlist_papers = dict(sorted(nonlist_papers.items(), key=lambda item: item[1]['mycount'])[:maximum])
+			#nonlist_papers = dict(itertools.islice(nonlist_papers.items(), maximum)) 
+		except Exception as e:
+			print("Error %s" % e)
+			pass 
+		assert len(nonlist_papers) <= maximum
+		# create nodes for the top 10
+		for nonlist_paper in nonlist_papers:
+			entry = nonlist_papers[nonlist_paper]
+			title = "no title"
+			if 'title' in entry:
+				title = entry['title']
+			G.add_node(nonlist_paper, title=title)
+		nonlist_papers_ind = list(nonlist_papers.keys())
+
+	# second pass, not the most efficient but the easiest to understand
 	for paper in papers:
-		# add the node
 		G.add_node(paper['id'], title=paper['title'], year=paper['year'], refcount=paper['ref_count'], citecount=paper['cite_count'], venue=paper['venue'])
 		for reference in paper["references"]:
-			# if reference is not in our list and also not in our nonlist list yet
-			if plot_everything and reference["id"] not in papers_ind and reference["id"] not in nonlist_papers_ind:
-				nonlist_papers_ind.append(reference['id'])
-				title = "no title"
-				if 'title' in reference:
-					title = reference['title']
-				G.add_node(reference['id'], title=title)
-			if plot_everything or reference["id"] in papers_ind:
+			if reference["id"] in papers_ind or reference["id"] in nonlist_papers:
 				G.add_edge(paper["id"], reference["id"])
 	
-
-
+	# make a mapping
 	idx_to_node_dict = {}
-	for idx, node in enumerate(papers_ind):
+	for idx, node in enumerate(G.nodes()):
 		idx_to_node_dict[idx] = node
-	print(papers_ind)
-	print(G.nodes())
+
+
+	# create a color map
+	color_map = []
+	for node in G:
+		if node in nonlist_papers:
+			color_map.append('green')
+		else:
+			color_map.append('blue')
 
 	options = {"edgecolors": "tab:gray", "node_size": 100, "alpha": 0.9}
 	fig, ax = plt.subplots()
 	pos = nx.spring_layout(G)
-	mynodes = nx.draw_networkx_nodes(G, pos=pos, ax=ax, nodelist=papers_ind, node_color="tab:red", label="string", **options)
-	allnodes = [mynodes]
-	if plot_everything:
-		nonlist_nodes = nx.draw_networkx_nodes(G, pos=pos, ax=ax, nodelist=nonlist_papers_ind, node_color="tab:blue", label="string", **options)
-		allnodes.append(nonlist_nodes)
-
+	mynodes = nx.draw_networkx_nodes(G, pos=pos, ax=ax, node_color=color_map, label="string", **options)
 	nx.draw_networkx_edges(G, pos=pos, ax=ax)
 	
 	annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
@@ -241,16 +333,15 @@ def visualize(papers, plot_everything=False, threshold=3):
 	def hover(event):
 		vis = annot.get_visible()
 		if event.inaxes == ax:
-			for nodess in allnodes:
-				cont, ind = nodess.contains(event)
-				if cont:
-					update_annot(ind)
-					annot.set_visible(True)
+			cont, ind = mynodes.contains(event)
+			if cont:
+				update_annot(ind)
+				annot.set_visible(True)
+				fig.canvas.draw_idle()
+			else:
+				if vis:
+					annot.set_visible(False)
 					fig.canvas.draw_idle()
-				else:
-					if vis:
-						annot.set_visible(False)
-						fig.canvas.draw_idle()
 
 
 
--- README.md
+++ README.md
@@ -8,6 +8,25 @@ Questions you can answer:
 * are there distinct clusters? (clusters of papers that never cite from the other cluster)
 * are there important papers?
 
+# Installation and Usage
+Requirement: python3 needs to be installed.
+Run 'pip3 requirements.txt' to install dependencies.
+Usage: 'usage: RCCV.py [-h] [-s src] [-v] [-f] [-a] [-m max_nr] [-t thr_nr] filename'
+Currently only supports items with a DOI.
+
+## CrossRef
+When using CrossRef please be polite and give some contact details!
+1. cp crapi_key.tmp .crapi_key
+Fill in the user agent and mailto.
+Remove API token if you don't have one.
+
 # Issues
 RCCV supports both CrossRef (based on DOI) and SemanticScholar (based on their identifier).
 SemanticScholar seems to offer the most complete coverage because DOI's are not always available.
+1. All bibtex items need a DOI
+
+# Roadmap
+1. Search by title
+2. Better graph representation
+3. More advanced coloring and visualization (for example group same venues together)
+4. Add API support for SemanticScholar
